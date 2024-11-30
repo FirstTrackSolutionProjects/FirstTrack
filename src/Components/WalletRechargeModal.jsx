efdb0e98ffad8e9d1c662e5552b507bafc1ced5b
AdityaKumar5155 <adityakumar5155@gmail.com>
2024-11-30 15:33:29 +0530

    chore : changed the minimum recharge amount to 500 after prod test

--- src/Components/WalletRechargeModal.jsx
+++ src/Components/WalletRechargeModal.jsx
@@ -111,7 +111,7 @@ const WalletRechargeModal = ({onClose}) => {
       <input
         type="number"
         value={amount}
-        min={1}
+        min={500}
         onChange={(e) => setAmount(e.target.value)}
         className='w-full border py-2 px-4 rounded-3xl'
       />
